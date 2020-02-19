42c15ff88d2cf832c873b86a8a2e940397e26f1a
Giovanni Spolidorio <spolidorio@gmail.com>
2020-02-19 19:03:11 -0300

    finalize feature/minMaxAge

--- tests/validations/age.spec.js
+++ tests/validations/age.spec.js
@@ -1,13 +1,139 @@
 import validator from '../../index.js'
 
-describe('Age', () => {
+const now = (value) =>{
+  let today = new Date()
+  let year = today.getFullYear() - value
+  return today.setFullYear(year)
+}
 
-  test('Validar idades maior que o valor ', () => {
-    validator.age('age', {
-      value: 18,
-      condition: '>=',
-      age: 18
-    })
+describe('Age', () => {
+  test('Validar se a idade é igual que o valor solicitado', () => {
+    const valid = [
+      {value: now(29), condition: '===', age: 29},
+      {value: now(29), condition: '===', age: 29},
+      {value: now(29), condition: '===', age: 29},
+      {value: now(29), condition: '===', age: 29}
+    ]
+    const invalid = [
+      {value: now(29), condition: '===', age: 30},
+      {value: now(29), condition: '===', age: 100},
+      {value: now(29), condition: '===', age: 1001},
+      {value: now(29), condition: '===', age: 28}
+    ]
+    
+    valid.forEach(value => expect(validator.age('age', value).valid).toBeTruthy());
+    invalid.forEach(value => expect(validator.age('age', value).valid).toBeFalsy());
   });
-  // valid.forEach(value => expect(validator.carPlate('carPlate', value).valid).toBeTruthy());
+
+  test('Validar se a idade é diferente que o valor solicitado', () => {
+    const valid = [
+      {value: now(12), condition: '!==', age: 45},
+      {value: now(1), condition: '!==', age: 29},
+      {value: now(5), condition: '!==', age: 29},
+      {value: now(10000), condition: '!==', age: 29}
+    ]
+    const invalid = [
+      {value: now(30), condition: '!==', age: 30},
+      {value: now(100), condition: '!==', age: 100},
+      {value: now(1001), condition: '!==', age: 1001},
+      {value: now(28), condition: '!==', age: 28}
+    ]
+    
+    valid.forEach(value => expect(validator.age('age', value).valid).toBeTruthy());
+    invalid.forEach(value => expect(validator.age('age', value).valid).toBeFalsy());
+  });
+
+  test('Validar se a idade é maior ou igual que o valor solicitado', () => {
+    const valid = [
+      {value: now(29), condition: '>=', age: 18},
+      {value: now(1), condition: '>=', age: 0},
+      {value: now(5), condition: '>=', age: 2},
+      {value: now(29), condition: '>=', age: 29}
+    ]
+    const invalid = [
+      {value: now(29), condition: '>=', age: 30},
+      {value: now(29), condition: '>=', age: 100},
+      {value: now(29), condition: '>=', age: 1001},
+      {value: now(29), condition: '>=', age: 30}
+    ]
+    
+    valid.forEach(value => expect(validator.age('age', value).valid).toBeTruthy());
+    invalid.forEach(value => expect(validator.age('age', value).valid).toBeFalsy());
+  });
+
+  test('Validar se a idade é maior que o valor solicitado', () => {
+    const valid = [
+      {value: now(29), condition: '>', age: 18},
+      {value: now(1), condition: '>', age: 0},
+      {value: now(5), condition: '>', age: 2},
+      {value: now(29), condition: '>', age: 18}
+    ]
+    const invalid = [
+      {value: now(29), condition: '>', age: 30},
+      {value: now(29), condition: '>', age: 100},
+      {value: now(29), condition: '>', age: 1001},
+      {value: now(29), condition: '>', age: 29}
+    ]
+    
+    valid.forEach(value => expect(validator.age('age', value).valid).toBeTruthy());
+    invalid.forEach(value => expect(validator.age('age', value).valid).toBeFalsy());
+  });
+
+  test('Validar se a idade é menor igual que o valor solicitado', () => {
+    const valid = [
+      {value: now(29), condition: '<=', age: 40},
+      {value: now(0), condition: '<=', age: 1},
+      {value: now(1), condition: '<=', age: 2},
+      {value: now(45), condition: '<=', age: 45}
+    ]
+    const invalid = [
+      {value: now(60), condition: '<=', age: 30},
+      {value: now(200), condition: '<=', age: 100},
+      {value: now(9000), condition: '<=', age: 1001},
+      {value: now(30), condition: '<=', age: 29}
+    ]
+    
+    valid.forEach(value => expect(validator.age('age', value).valid).toBeTruthy());
+    invalid.forEach(value => expect(validator.age('age', value).valid).toBeFalsy());
+  });
+
+  test('Validar se a idade é menor que o valor solicitado', () => {
+    const valid = [
+      {value: now(29), condition: '<', age: 40},
+      {value: now(0), condition: '<', age: 1},
+      {value: now(1), condition: '<', age: 2},
+      {value: now(44), condition: '<', age: 45}
+    ]
+    const invalid = [
+      {value: now(60), condition: '<', age: 30},
+      {value: now(200), condition: '<', age: 100},
+      {value: now(9000), condition: '<', age: 1001},
+      {value: now(30), condition: '<', age: 29}
+    ]
+    
+    valid.forEach(value => expect(validator.age('age', value).valid).toBeTruthy());
+    invalid.forEach(value => expect(validator.age('age', value).valid).toBeFalsy());
+  });
+
+  test('Validar se a idade é menor que o valor solicitado', () => {
+    const valid = [
+      {value: now(29), condition: '<', age: 40},
+      {value: now(0), condition: '<', age: 1},
+      {value: now(1), condition: '<', age: 2},
+      {value: now(44), condition: '<', age: 45}
+    ]
+    const invalid = [
+      {value: now(60), condition: '<', age: 30},
+      {value: now(200), condition: '<', age: 100},
+      {value: now(9000), condition: '<', age: 1001},
+      {value: now(30), condition: '<', age: 29}
+    ]
+    
+    valid.forEach(value => expect(validator.age('age', value).valid).toBeTruthy());
+    invalid.forEach(value => expect(validator.age('age', value).valid).toBeFalsy());
+  });
+
+  test('Validar se o parametro `condition` esta fora do padrão do componente', () => {
+    expect(validator.age('age', {value: now(1), condition: '======', age: 29}).valid).toBeFalsy();
+  });
 });
--- validations/age.js
+++ validations/age.js
@@ -3,8 +3,24 @@ import utils from "../utils/utils";
 export default (name, {value, condition, age}) => {
   let field = {
     name: name,
-    valid: false
+    valid: true
   }
-  // '===', '>=', '<=', '<', '>', '!=='
+
+  const birthDate = new Date(value);
+  const today = new Date();
+  const localAge = today.getFullYear() - birthDate.getFullYear();
+  const month = today.getMonth() - birthDate.getMonth();
+
+  const validateAge = {
+    '===': () => localAge === age,
+    '!==': () => localAge !== age,
+    '>=': () => localAge >= age,
+    '>': () => localAge > age,
+    '<=': () => localAge <= age,
+    '<': () => localAge < age,
+    'default': () => false
+  }
+
+  field.valid = (validateAge[condition] || validateAge['default'])()
   return field
 }
